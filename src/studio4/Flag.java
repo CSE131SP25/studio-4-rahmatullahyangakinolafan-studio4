2fa1eb716a267c6fa55bb12d126682bf29169e18
fanha <fanha@Hannah-PC.dhcp.wustl.edu>
2025-02-19 14:21:07 -0600

    yayayayayPOPPIII

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,75 @@
 package studio4;
 
 import java.awt.Color;
+import java.awt.Font;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		
+		StdDraw.setPenColor(158, 158, 158);
+		
+		double [] x = {0.2, 0.3, 0.3};
+		double [] y = {0.8, 0.8, 0.9};
+		
+		StdDraw.filledPolygon(x, y);
+		
+		double [] x2 = {0.7, 0.8, 0.7};
+		double [] y2 = {0.8, 0.8, 0.9};
+		
+		StdDraw.filledPolygon(x2, y2);
+		
+		double [] x3 = {0.2, 0.3, 0.3};
+		double [] y3 = {0.2, 0.1, 0.2};
+		double [] x4 = {0.7, 0.8, 0.7};
+		double [] y4 = {0.1, 0.2, 0.2};
+		
+		StdDraw.filledPolygon(x3, y3);
+		StdDraw.filledPolygon(x4, y4);
+		
+		
+		StdDraw.filledRectangle(0.5, 0.85, 0.2, 0.05);
+		StdDraw.filledRectangle(0.5, 0.15, 0.2, 0.05);
+		
+		StdDraw.setPenColor(255, 8, 127);
+		StdDraw.filledRectangle(0.5, 0.5, 0.3, 0.3);
+		
+		
+		StdDraw.setPenColor(255, 255, 255);
+		Font font = new Font ("Arial", Font.BOLD, 60);
+		StdDraw.setFont(font);
+		StdDraw.text(0.45, 0.73, "POPPI");
+		
+		StdDraw.setPenColor(255, 250, 0);
+		
+		StdDraw.filledCircle(0.6, 0.4, 0.15);
+		
+		StdDraw.filledEllipse(0.57, 0.5772, 0.05, 0.025);
+		StdDraw.filledEllipse(0.67, 0.5772, 0.05, 0.025);
+		
+		
+		
+		StdDraw.circle(0.35, 0.64, 0.05);
+		StdDraw.circle(0.35, 0.45, 0.05);
+		StdDraw.circle(0.35, 0.26, 0.05);
+		
+		
+		Font fontnew = new Font ("Arial", Font.BOLD, 45);
+		StdDraw.setFont(fontnew);
+		
+		StdDraw.text(0.35, 0.64, "1");
+		StdDraw.text(0.35, 0.45, "3");
+		StdDraw.text(0.35, 0.26, "1");
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,47 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shape = in.next();
+		int Red = in.nextInt();
+		int Green = in.nextInt();
+		int Blue = in.nextInt();
+		Boolean filled = in.nextBoolean();
+		
+		
+		
+		StdDraw.setPenColor(Red,Green,Blue);
+		if(filled) {
+			if(shape.equals("rectangle")){
+				Double X = in.nextDouble();
+				Double Y = in.nextDouble();
+				Double halfWidth = in.nextDouble();
+				Double halfLength = in.nextDouble();
+				StdDraw.filledRectangle(X, Y, halfWidth, halfLength);
+			
+		}
+			else if(shape.equals("ellipse")) {
+				Double X = in.nextDouble();
+				Double Y = in.nextDouble();
+				Double halfWidth = in.nextDouble();
+				Double halfLength = in.nextDouble();
+				StdDraw.filledEllipse(X, Y, halfWidth, halfLength);
+				
+			} else if(shape.equals("triangle")) {
+				double[] x = new double[3];
+				double[] y = new double[3];
+				x[0] = in.nextDouble();
+				x[1] = in.nextDouble();
+				x[2] = in.nextDouble();
+				y[0] = in.nextDouble();
+				y[1] = in.nextDouble();
+				y[2] = in.nextDouble();
+				StdDraw.filledPolygon(x,y);
+				
+			}
+			
+		
+		
 	}
+		
+}
 }
